4eda29a53ddce5b6b8f0241b00abecb5fa71cf22
bjmt <benjmtremblay@gmail.com>
2019-04-02 18:01:04 -0400

    fixed examples

--- inst/script/example-markdown.R
+++ inst/script/example-markdown.R
@@ -1,29 +1,29 @@
 #!/usr/bin/env Rscript --quiet
 
-autobench::begin("../extdata/example-markdown.md", "Example markdown output",
-                 tool = "bench", format = "md")
+autobenchR::begin("../extdata/example-markdown.md", "Example markdown output",
+                  tool = "bench", format = "md")
 
-autobench::run("Tests",
-               e1 = runif(100000, 0, 10),
-               e2 = rnorm(100000, 5, 2.5))
+autobenchR::run("Tests",
+                e1 = runif(100000, 0, 10),
+                e2 = rnorm(100000, 5, 2.5))
 
-autobench::update(tool = "microbenchmark")
-autobench::run("Different tool",
-               e1 = runif(100000, 0, 10),
-               e2 = rnorm(100000, 5, 2.5))
+autobenchR::update(tool = "microbenchmark")
+autobenchR::run("Different tool",
+                e1 = runif(100000, 0, 10),
+                e2 = rnorm(100000, 5, 2.5))
 
-autobench::update(tool = "rbenchmark", unit = "ms")
-autobench::run("Different tool 2",
-               e1 = runif(100000, 0, 10),
-               e2 = rnorm(100000, 5, 2.5))
+autobenchR::update(tool = "rbenchmark", unit = "ms")
+autobenchR::run("Different tool 2",
+                e1 = runif(100000, 0, 10),
+                e2 = rnorm(100000, 5, 2.5))
 
-autobench::skip()
-autobench::run("Skipped tests",
-               e1 = sample(1:1000, 1000))
+autobenchR::skip()
+autobenchR::run("Skipped tests",
+                e1 = sample(1:1000, 1000))
 
-autobench::note("* Bullet point 1", "* Bullet point 2", name = "Some notes")
+autobenchR::note("* Bullet point 1", "* Bullet point 2", name = "Some notes")
 
-autobench::run("Failing test",
-               e1 = 1 + "a")
+autobenchR::run("Failing test",
+                e1 = 1 + "a")
 
-autobench::end()
+autobenchR::end()
